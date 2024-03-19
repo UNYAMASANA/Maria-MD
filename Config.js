55b9a0364871bad74e88f0c5e4716dfb112aebf4
UNYAMASANA <163345589+UNYAMASANA@users.noreply.github.com>
2024-03-19 15:05:20 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
+global.ownernumber = ['255622615137']
+global.ownername = "♜𝙼𝙾𝙰𝙻𝙻𝚈 -𝙼𝙳♜"//owner name
+global.ytname = "YT: ♜𝙼𝙾𝙰𝙻𝙻𝚈 -𝙼𝙳♜.inc"
+global.socialm = "GitHub: ♜𝙼𝙾𝙰𝙻𝙻𝚈 -𝙼𝙳♜"
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = '♜𝙼𝙾𝙰𝙻𝙻𝚈 -𝙼𝙳♜' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '♜𝙼𝙾𝙰𝙻𝙻𝚈 -𝙼𝙳♜'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = '♜𝙼𝙾𝙰𝙻𝙻𝚈 -𝙼𝙳♜'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
@@ -30,7 +30,7 @@ global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = false //auto update bio
+global.autobio = true //auto update bio
 global.anti212 = true //auto block +212
 global.autoread_status = false //auto view status/story
 
